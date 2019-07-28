8c0b265ee15a8c423dd5b738742dfff70ef12872
Matt Wilkie <mattwilkie@Matts-MBP.broadband>
2019-07-28 13:58:38 +0100

    Created Game Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,17 @@
 //console.log("Up and running!");
 
-var cardOne = 'Queen';
-var cardTwo = 'Queen';
-var cardThree = 'King';
-var cardFour = 'King';
+var cards = ['Queen', 'Queen', 'King', 'King'];	
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[1] === cardsInPlay[0]) {alert("you found a match")}
+	else {alert("No Match")}
+}
